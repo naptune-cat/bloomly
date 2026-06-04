c83dbcb095d8c508cb2c99ed43aec683dd0483a4
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-06-04 18:57:33 +0530

    feat: added User Entity

--- bloomly/backend/src/main/java/com/zoya/bloomly/entity/user.java
+++ bloomly/backend/src/main/java/com/zoya/bloomly/entity/user.java
@@ -1,10 +1,32 @@
 package com.zoya.bloomly.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = "users")
-public class user {
-    
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
 }
